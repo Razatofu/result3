b73c2a8f374ea67f9fe651962fc33a54d8bfb9f2
raza ayaz <razaayaz@yahoo.com>
2024-12-29 21:12:57 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+var a = 2, b = 1;
+console.log("variable a:" +a); 2
+console.log("variable b:" +b); 1
+
+
+// console.log("the value of --a:" + --a); 1
+// console.log("the value result of --a - --b :" + (--a - --b)); 0
+// console.log("the value result of --a - --b + ++b:" + (--a - --b) + (++b));
+// console.log("the value for --a - --b + ++b + b--;" + (--a - --b) + + (++b + b--));
+
+var result = --a - --b + ++b + b--;
+result = --a - --b + ++b + b--;
+result = 1 - 0 + 1 + 1;
+result = 1 - 0 + 1 + 1;
+result = 1 + 1 + 1;
+result = 3; 
+console.log("the result :" + result);
+
